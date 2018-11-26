1801cf1fada3ba1d457000fd3bbdd2a38a5b6920
Quantum <quantum2048@gmail.com>
2018-11-26 01:51:26 -0500

    Initial commit: most of the UI

---
+++ src/clipboard.js
@@ -0,0 +1,18 @@
+const Clipboard = require('clipboard/dist/clipboard')
+import $ from 'jquery/dist/jquery'
+import 'popper.js'
+import 'bootstrap/js/dist/tooltip'
+
+$(() => {
+  function showTooltip ($element, message) {
+    $element.tooltip('dispose')
+      .attr('data-original-title', message)
+      .tooltip('show')
+
+    setTimeout(() => $element.tooltip('dispose'), 1000)
+  }
+
+  new Clipboard('.copy', {})
+    .on('success', e => showTooltip($(e.trigger), 'Copied!'))
+    .on('error', e => showTooltip($(e.trigger), 'Failed to copy!'))
+})
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/app.js',
+  mode: process.env.NODE_ENV || 'development',
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+        options: {
+          interpolate: true
+        }
+      },
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            // Adds CSS to the DOM by injecting a `<style>` tag
+            loader: 'style-loader'
+          },
+          {
+            // Interprets `@import` and `url()` like `import/require()` and will resolve them
+            loader: 'css-loader'
+          },
+          {
+            // Loader for webpack to process CSS with PostCSS
+            loader: 'postcss-loader',
+            options: {
+              plugins: function () {
+                return [
+                  require('autoprefixer')
+                ]
+              }
+            }
+          },
+          {
+            // Loads a SASS/SCSS file and compiles it to CSS
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
+  }
+}
